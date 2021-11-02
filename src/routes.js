a83e19edf737dc634dc498c6e8d78b6ebb1d7cee
Matheus Motta <matheusdomingues423@gmail.com>
2021-11-02 13:02:53 -0300

    commit on 'main'

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,34 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mysql = require('mysql');
-// const { response } = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const { response } = require('express');
 
-// const connection = mysql.createPool({  // conexão com o banco 
+const connection = mysql.createPool({  // conexão com o banco 
 
-//     host: 'localhost',
-//     user: 'root',
-//     password: null,
-//     database: 'projeto_02'
-// });
+    host: 'localhost',
+    user: 'root',
+    password: null,
+    database: 'projeto_02'
+});
 
-// const app = express();
+const app = express();
 
-// app.get('/tb_adminalunos', function(req, res){  //conexão com a tabela de alunos 
+app.get('/tb_adminalunos', function(req, res){  //conexão com a tabela de alunos 
 
-//     connection.getConnection(function(err, connection){ 
+    connection.getConnection(function(err, connection){ 
 
-//         connection.query('SELECT * FROM `tb_adminalunos`', function(error, results, fields){ //vai fazer uma consulta na tabela
+        connection.query('SELECT * FROM `tb_adminalunos`', function(error, results, fields){ //vai fazer uma consulta na tabela
 
-//                 if(error) throw error; //se tiver erro ele vai avisar
-//                 res.send(results); // se não tiver, vai retornar a resposta
-//         });
-//     });
-// });
+                if(error) throw error; //se tiver erro ele vai avisar
+                res.send(results); // se não tiver, vai retornar a resposta
+        });
+    });
+});
 
-// app.listen(3333, ()=>{ // entrar pela porta 3333
+app.listen(3333, ()=>{ // entrar pela porta 3333
 
-//     console.log('Entre na porta');
-// });
+    console.log('Entre na porta');
+});
 
 // function Connect(){
 
