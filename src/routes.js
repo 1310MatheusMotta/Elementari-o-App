c51d01578b7e9157c78dc412060ac897af5c96fc
Matheus Motta <matheusdomingues423@gmail.com>
2021-11-13 07:59:19 -0300

    commit on 'main'

--- src/routes.js
+++ src/routes.js
@@ -11,19 +11,19 @@ const connection = mysql.createPool({  // conexão com o banco
 
 const app = express();
 
-app.get('/alunos', function(req, res){  //conexão com a tabela de alunos 
+app.get('/alunos', function(req, res){ 
 
     connection.getConnection(function(err, connection){ 
 
-        connection.query('SELECT * FROM `alunos`', function(error, results){ //vai fazer uma consulta na tabela
+        connection.query('SELECT * FROM `alunos`', function(error, results){ // consulta na tabela
 
-                // if(error) throw error; //se tiver erro ele vai avisar
-                res.send(results); // se não tiver, vai retornar a resposta
+                // if(error) throw error; 
+                res.send(results); 
         });
     });
 });
 
-app.listen(3333, ()=>{ // entrar pela porta 3333
+app.listen(3333, ()=>{ 
 
     console.log('Entre na porta');
 });
